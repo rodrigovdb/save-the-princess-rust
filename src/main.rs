5709d414696520e92c76f6bf5891d4fb82b4189f
Rodrigo Otavio van den Berg Maia <rodrigo@devlandia.net>
2018-04-05 17:59:51 -0300

    Tests

--- src/main.rs
+++ src/main.rs
@@ -11,39 +11,53 @@ impl Character {
     self.row != peach.row || self.col != peach.col
   }
 
-  fn move_himself(&mut self, peach: &Character) {
+  fn move_himself(&mut self, peach: &Character) -> String {
+    let mut response = String::new();
+
     if self.col != peach.col {
-      self.move_horizontal(peach);
+      response.push_str(&self.move_horizontal(peach));
     }
+
     if self.row != peach.row {
-      self.move_vertical(peach);
+      response.push_str(&self.move_vertical(peach));
     }
+
+    response
   }
 
-  fn move_horizontal(&mut self, peach: &Character) {
+  fn move_horizontal(&mut self, peach: &Character) -> String {
+    let mut response = String::new();
+
     if self.col > peach.col {
       self.col -= 1;
-      println!("LEFT");
+      response.push_str("LEFT\n");
     }
     else {
       self.col += 1;
-      println!("RIGHT");
+      response.push_str("RIGHT\n");
     }
+
+    response
   }
 
-  fn move_vertical(&mut self, peach: &Character) {
+  fn move_vertical(&mut self, peach: &Character) -> String {
+    let mut response = String::new();
+
     if self.row > peach.row {
       self.row -= 1;
-      println!("UP");
+      response.push_str("UP\n");
     }
     else {
       self.row += 1;
-      println!("DOWN");
+      response.push_str("DOWN\n");
     }
+
+    response
   }
 }
 
 fn show_board(board: &[&str]){
+  println!("\n");
   for x in board { println!("{}", x) }
 }
 
@@ -69,16 +83,22 @@ fn discover_character(board: &[&str], character: char) -> Character {
   }
 }
 
-fn play(board: &[&str]){
+fn play(board: &[&str]) -> String {
   let mut mario   = discover_character(&board, 'm');
-  let peach    = discover_character(&board, 'p');
+  let peach       = discover_character(&board, 'p');
 
-  println!("\n");
-  show_board(&board);
+  //println!("\n");
+  //show_board(&board);
+  let mut response  = String::new();
 
   while mario.should_move(&peach) {
-    mario.move_himself(&peach);
+    response.push_str(&mario.move_himself(&peach));
   }
+
+  let len = response.len();
+  response.truncate(len - 1);
+
+  response
 }
 
 fn build_board(board: &str) -> Vec<&str> {
@@ -87,10 +107,10 @@ fn build_board(board: &str) -> Vec<&str> {
   parts
 }
 
-fn displayPathtoPrincess(_n: i32, raw_board: &str) {
+fn displayPathtoPrincess(_n: i32, raw_board: &str) -> String {
   let board: Vec<&str> = build_board(raw_board);
 
-  play(&board);
+  play(&board)
 }
 
 fn main() {
@@ -108,6 +128,53 @@ fn main() {
   ];
 
   for board in &boards {
-    displayPathtoPrincess(n, board);
+    show_board(&build_board(board));
+
+    println!("{}", displayPathtoPrincess(n, board));
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn right_right() {
+    assert_eq!("RIGHT\nRIGHT", displayPathtoPrincess(1, "---\nm-p\n---"));
+  }
+
+  #[test]
+  fn left_left() {
+    assert_eq!("LEFT\nLEFT", displayPathtoPrincess(1, "---\np-m\n---"));
+  }
+
+  #[test]
+  fn down_down() {
+    assert_eq!("DOWN\nDOWN", displayPathtoPrincess(1, "m--\n---\np--"));
+  }
+
+  #[test]
+  fn up_up() {
+    assert_eq!("UP\nUP", displayPathtoPrincess(1, "--p\n---\n--m"));
+  }
+
+  #[test]
+  fn left_down() {
+    assert_eq!("LEFT\nDOWN", displayPathtoPrincess(1, "---\n-m-\np--"));
+  }
+
+  #[test]
+  fn left_up() {
+    assert_eq!("LEFT\nUP", displayPathtoPrincess(1, "p--\n-m-\n---"));
+  }
+
+  #[test]
+  fn left_up_left_up() {
+    assert_eq!("LEFT\nUP\nLEFT\nUP", displayPathtoPrincess(1, "p--\n---\n--m"));
+  }
+
+  #[test]
+  fn left_down_left_down() {
+    assert_eq!("LEFT\nDOWN\nLEFT\nDOWN", displayPathtoPrincess(1, "--m\n---\np--"));
   }
 }
